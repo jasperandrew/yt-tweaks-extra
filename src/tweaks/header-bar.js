a4050c7e83f17567583f0fc6435f8c490cc58bda
Jasper Andrew <jqaxyz@gmail.com>
2026-07-24 23:04:29 -0400

    Add option to hide search bar, or only on watch page

--- src/tweaks/header-bar.js
+++ src/tweaks/header-bar.js
@@ -170,4 +170,14 @@ ytTweaks.tweaks.push(function (settings) {
       display: none !important;
     }
     `;
+
+    if (settings.hideSearchBar) {
+        const scope = settings.hideSearchBarWatchOnly ? '#masthead[is-watch-page] ' : '';
+
+        ytTweaks.sheet.textContent += `
+        ${scope}#center.ytd-masthead {
+          display: none !important;
+        }
+        `;
+    }
 });
